56d6803e101f557606dc8fcb6e70729530d00709
alvinwmtan <alvin.tan1996@gmail.com>
2023-12-18 14:53:01 -0800

    added fscore correlation to CV

--- scripts/03-sublist-evaluation.R
+++ scripts/03-sublist-evaluation.R
@@ -37,3 +37,44 @@ get_sumscore_cor <- function(sublist, xldf, all_prod, lang) {
     cor(rowMeans(all_prod[[lang]], na.rm = TRUE), prod_sub,
       use = "na.or.complete"))
 }
+
+get_fscore_cor <- function(sublist, xldf, lang, full_fscores) {
+  full_model <- readRDS(glue("data/prod_models/{lang}_2PL_allforms_prod_fits.rds"))
+  xldf_sub <- sublist |> 
+    left_join(xldf |> filter(language == lang),
+              by = "uni_lemma",
+              multiple = "first") |> 
+    filter(!is.na(uid))
+  
+  params_orig <- mod2values(full_model$model)
+  
+  new_vals <- params_orig |> 
+    filter(item %in% xldf_sub$uid) |> 
+    select(item, name, value) |> 
+    pivot_wider(names_from = name, values_from = value) |> 
+    mutate(a1 = 1) |> 
+    select(-d) |> 
+    left_join(xldf_sub |> select(uid, uni_lemma, mean_d), by = c("item" = "uid")) |> 
+    rename(d = mean_d) |> 
+    pivot_longer(cols = c(a1, d, g, u)) |> 
+    select(-uni_lemma)
+  
+  params_new <- params_orig |> 
+    left_join(new_vals, by = c("item", "name")) |> 
+    mutate(value = coalesce(value.y, value.x), .after = parnum) |> 
+    mutate(est = FALSE,
+           item = `class<-`(item, value = "character")) |> 
+    select(-value.x, -value.y)
+  
+  swad_mod <- mirt(full_model$model@Data$data, 1, pars = params_new)
+  
+  non_swad_ids <- setdiff(colnames(full_model$model@Data$data), xldf_sub$uid)
+  masked_resps <- full_model$model@Data$data
+  masked_resps[, non_swad_ids] <- NA
+  swad_fscores <- fscores(swad_mod, 
+                          response.pattern = masked_resps, 
+                          method = "MAP")[,1] 
+  
+  cor(full_fscores, swad_fscores, use = "complete.obs")
+}
+
